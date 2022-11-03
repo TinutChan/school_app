b98e49924103fbd1910a0b6848abefbf224b993a
TinutChan <tinutchan444@gmail.com>
2022-11-03 17:12:20 +0700

    test

--- lib/home/screens/homescreen.dart
+++ lib/home/screens/homescreen.dart
@@ -1,4 +1,4 @@
-import 'package:api_app/widgets/custom_button.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,45 +9,136 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final CarouselController _controller = CarouselController();
+  int _current = 0;
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomButton(
-            borderSidewidth: 2,
-            borderWidthColor: Colors.grey,
-            borderstyle: BorderStyle.solid,
-            bgcolor: Colors.green,
-            icons: Icons.home,
-            label: 'home',
-            onPressed: () {
-              debugPrint('==============hello=============');
-            },
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            width: double.infinity,
+            height: 150,
+            child: CarouselSlider(
+              options: CarouselOptions(
+                scrollDirection: Axis.horizontal,
+                aspectRatio: 2.0,
+                viewportFraction: 1,
+                initialPage: 1,
+                autoPlay: true,
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
+              items: imgList
+                  .map(
+                    (item) => Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        image: DecorationImage(
+                            image: NetworkImage(item), fit: BoxFit.cover),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
           ),
-          const CustomButton(
-            borderSidewidth: 2,
-            borderWidthColor: Colors.grey,
-            borderstyle: BorderStyle.solid,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((entry) {
+              return GestureDetector(
+                onTap: () => _controller.animateToPage(entry.key),
+                child: Container(
+                  width: 7.0,
+                  height: 7.0,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 0.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.green.shade900)
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4),
+                  ),
+                ),
+              );
+            }).toList(),
           ),
-          const CustomButton(
-            borderSidewidth: 2,
-            borderWidthColor: Colors.grey,
-            borderstyle: BorderStyle.solid,
+          Container(
+            margin: const EdgeInsets.only(
+                left: 10, top: 12.0, right: 10, bottom: 8.0),
+            height: 100,
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                blurRadius: 1,
+                color: Colors.grey.withOpacity(0.3),
+                offset: const Offset(0.1, 0.1),
+                spreadRadius: 0,
+              )
+            ], borderRadius: BorderRadius.circular(10.0), color: Colors.white),
           ),
-          const CustomButton(
-            borderSidewidth: 2,
-            borderWidthColor: Colors.grey,
-            borderstyle: BorderStyle.solid,
-          ),
-          const CustomButton(
-            borderSidewidth: 2,
-            borderWidthColor: Colors.grey,
-            borderstyle: BorderStyle.solid,
-          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                const Text(
+                  'Recommended',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                const Spacer(),
+                GestureDetector(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      const Text(
+                        'See More',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      const SizedBox(
+                        width: 7.0,
+                      ),
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward_ios,
+                          size: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
